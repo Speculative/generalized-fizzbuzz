5380af2ae883a22eabc4910a72283696321dc247
Jeff Tao <jatao88@gmail.com>
2022-08-26 10:00:57 -0400

    Real monoids

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,7 +7,7 @@ where
 fizzbuzz :: [String]
 fizzbuzz =
   map
-    ( applyRulesString
+    ( applyRules
         [ (\n -> n `mod` 3 == 0, const "fizz"),
           (\n -> n `mod` 5 == 0, const "buzz")
         ]
@@ -18,7 +18,7 @@ fizzbuzz =
 levinFizzbuzz :: [String]
 levinFizzbuzz =
   map
-    ( applyRulesString
+    ( applyRules
         [ (\n -> n `mod` 7 == 0, const "fizz"),
           (contains7, const "buzz")
         ]
@@ -30,14 +30,11 @@ contains7 :: Integer -> Bool
 contains7 0 = False
 contains7 n = (n `mod` 10 == 7) || contains7 (n `div` 10)
 
-applyRulesString :: [(a -> Bool, a -> String)] -> (a -> String) -> a -> String
-applyRulesString = applyRules (++) ""
-
-applyRules :: (b -> b -> b) -> b -> [(a -> Bool, a -> b)] -> (a -> b) -> a -> b
-applyRules combine empty allRules fallback val = applyRules' allRules False
+applyRules :: Monoid b => [(a -> Bool, a -> b)] -> (a -> b) -> a -> b
+applyRules allRules fallback val = applyRules' allRules False
   where
     applyRules' ((predicate, result) : rules) matchedAny
-      | predicate val = result val `combine` applyRules' rules True
+      | predicate val = result val <> applyRules' rules True
       | otherwise = applyRules' rules matchedAny
-    applyRules' [] True = empty
+    applyRules' [] True = mempty
     applyRules' [] False = fallback val
